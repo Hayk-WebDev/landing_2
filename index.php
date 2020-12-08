55e460b143781a0d4b87972536cacde02d3a1b3b
Hayk-WebDev <68122023+Hayk-WebDev@users.noreply.github.com>
2020-12-08 19:24:49 +0400

    Update index.php

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ include("connect/lang.php");
             </div>
 
 
-             <ol class="carousel-indicators position-relative d-block">
+             <ol class="carousel-indicators position-relative d-block mt-lg-2 mt-5">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
